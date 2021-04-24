4f3ee955bd8926a88c6d353349634a8c795a6082
Ross White <rosswhite@outlook.com>
2021-04-24 16:17:52 +0100

    FEAT: Adding questions

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How is your applicaton installed?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license does your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+    },
+    {
+        type: 'input',
+        name: 'contributers',
+        message: 'Who contributed to your project?',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
