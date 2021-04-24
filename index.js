5c6dca57cb087a334d89f5569df69a4360c2132e
Ross White <rosswhite@outlook.com>
2021-04-24 16:19:20 +0100

    FEAT: Adding writeToFile and init funtions

--- index.js
+++ index.js
@@ -45,10 +45,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        writeToFile('README.md', generateMarkdown({...inquirerResponses }));
+    });
+}
 
 // Function call to initialize app
 init();
